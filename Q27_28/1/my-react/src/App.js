a0e459d1978170f112ba10284536d3546c1adbba
Salman Azhar <salmanazhar46@gmail.com>
2025-03-21 21:37:19 +0700

    Q27_28_1

---
+++ Q27_28/1/my-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h4>Ini My React</h4>
+    </div>
+  );
+}
+
+export default App;
