d435527047395bc44882bbb28cb5499a1748be2a
Salman Azhar <salmanazhar46@gmail.com>
2025-09-22 22:51:17 +0700

    2 fix

--- Q31_32/2/src/Home.jsx
+++ Q31_32/2/src/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ background: "#fff", padding: "30px", borderRadius: "10px", boxShadow: "0 4px 12px rgba(0,0,0,0.1)" }}>
       <h2 style={{ marginBottom: "10px", color: "#111" }}>Halaman Utama</h2>
-      <p style={{ color: "#555" }}>Selamat datang di aplikasi demo React Router dengan Nested Routing.</p>
+      <p style={{ color: "#555" }}>Selamat datang di aplikasi demo React Router karya salman!.</p>
     </div>
   );
 }
